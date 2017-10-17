20f3532aaab953ea27db1de8b56a5c6b80faca8a
Danielis Joniskis <g00333859@gmit.ie>
2017-10-17 19:26:27 +0100

    served a page using Bootstrap

--- guessingGame.go
+++ guessingGame.go
@@ -4,21 +4,23 @@
 package main
 
 import (
-	"fmt"
+//	"fmt"
 	"net/http"
 )
 
 //A http.ResponseWriter assembles the HTTP server's response by writing to it.
 //A http.Request is a data structure that represents the client HTTP request.
+/*
 func handler(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Set("Content-Type","text/html")
     fmt.Fprintln(w, "<h1>Guessing game</h1>")
 }
+*/
 
 //The main function begins with a call to http.HandleFunc.
 //It then calls http.ListenAndServe, specifying that it should listen on port 8080.
 func main() {
-    http.HandleFunc("/", handler)
+    http.Handle("/",  http.FileServer(http.Dir("./")))//serving index.html in the root directory.
     http.ListenAndServe(":8080", nil)
 }
