430a7ee4aeb8d30ea2254e408061502ba7295ed0
Danielis Joniskis <g00333859@gmit.ie>
2017-10-17 18:01:42 +0100

    Added some comments

--- guessingGame.go
+++ guessingGame.go
@@ -1,3 +1,6 @@
+//A web application in Go.
+//Author: Danielis Joniskis
+
 package main
 
 import (
@@ -5,11 +8,14 @@ import (
 	"net/http"
 )
 
+//A http.ResponseWriter assembles the HTTP server's response by writing to it.
+//A http.Request is a data structure that represents the client HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Guessing game")
 }
 
-
+//The main function begins with a call to http.HandleFunc.
+//It then calls http.ListenAndServe, specifying that it should listen on port 8080.
 func main() {
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
