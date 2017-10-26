82986f31386af159a466b7246749af8c60bb5f3a
Danielis Joniskis <g00333859@gmit.ie>
2017-10-26 16:18:24 +0100

    added template

--- guessingGame.go
+++ guessingGame.go
@@ -4,36 +4,35 @@
 package main
 
 import (
-	"fmt"
+ //   "fmt"
 	"net/http"
-    "html/template"
-    "os"
+    "text/template"
 )
 
 //A http.ResponseWriter assembles the HTTP server's response by writing to it.
 //A http.Request is a data structure that represents the client HTTP request.
-/*
-func handler(w http.ResponseWriter, r *http.Request) {
+
+/*func handler(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Set("Content-Type","text/html")
     fmt.Fprintln(w, "<h1>Guessing game</h1>")
-}
-*/
+}*/
 
 type Template struct {
     Message string
 }
 
-//The main function begins with a call to http.HandleFunc.
+//Adapted from https://www.youtube.com/watch?v=GTSq1VPPFco&feature=youtu.be.
+func templateHandler(w http.ResponseWriter, r *http.Request){
+    t, _ := template.ParseFiles("guess.html")
+    t.Execute(w, Template{Message:"Guess a number between 1 and 20"})
+}
+
+
+//The main function begins with a call to http.Handle.
 //It then calls http.ListenAndServe, specifying that it should listen on port 8080.
 func main() {
-    //http.HandleFunc("/", handler)
     http.Handle("/",  http.FileServer(http.Dir("./")))//serving index.html in the root directory.
+    http.HandleFunc("/guess", templateHandler)
     http.ListenAndServe(":8080", nil)
-
-     t := template.New("Template")
-     t, _ = t.Parse("<h2>Guess a number between 1 and 20</h2> {{.Message}}!")
-     t.Execute(os.Stdout)
-
-
 }
